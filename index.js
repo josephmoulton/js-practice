329a46c015dec4bd2a331d6c6e437d68bab42c8c
Joe <josephlewismoulton@gmail.com>
2022-10-17 22:55:24 +0100

    Java practice compelte -- not showing much as individual tasks completed

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-//console.log(fetch("https://jsonplaceholder.typicode.com/users/1"));
-const emailRef = document.querySelector(".email");
+async function firstSixIncomplete(uid) {
+  const promise = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const result = await promise.json();
+  const posts = result.filter((element) => element.completed == false);
+  const slicedArray = posts.slice(0, 6);
 
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((data) => {
-//     console.log(data);
-//     emailRef.innerHTML = data.email;
-//   });
+  console.log(slicedArray);}
 
-async function main() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-  const data = await response.json()
-  console.log(data)
-  emailRef.innerHTML = data.email
-}
-
-main()
+ firstSixIncomplete();
